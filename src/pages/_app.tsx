d70fb8bbe91bc22635f6579b12df685636583da0
Antonio Raposo <antoniocarlosraposo93@gmail.com>
2021-05-17 17:36:27 -0300

    adding eslint and prettier to project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
-import {AppProps} from 'next/app'
-import '../styles/global.scss'
+import { AppProps } from 'next/app';
+import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-  <>
-    <Component {...pageProps} />
-  </>
-  )
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
