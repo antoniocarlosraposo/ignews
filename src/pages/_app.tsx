92e069b3e88cf5a054d5680f13a73a44d01688e8
Antonio Raposo <antoniocarlosraposo93@gmail.com>
2021-05-18 15:45:33 -0300

    adding Header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import { AppProps } from 'next/app';
 import '../styles/global.scss';
+import { Header } from '../components/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Header />
       <Component {...pageProps} />
     </>
   );
